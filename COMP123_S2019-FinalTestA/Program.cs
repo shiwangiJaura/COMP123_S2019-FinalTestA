144c0099c2eae2ff312aded641e6d1e5c6e4fd97
301044130 <301044130@PR-E216-46>
2019-08-09 09:45:36 -0400

    added master form

--- COMP123_S2019-FinalTestA/Program.cs
+++ COMP123_S2019-FinalTestA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using COMP123_S2019_FinalTestA.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,22 @@ namespace COMP123_S2019_FinalTestA
 {
     static class Program
     {
+        //this temporary
+        public static MasterForm masterForm;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            masterForm = new MasterForm();
+
+            Application.Run(masterForm);
         }
     }
 }
